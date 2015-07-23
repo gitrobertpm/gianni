cc16cf2a2664582f4b6ee1d77b430dda14808487
Robert Manolis <robertpm@comcast.net>
2015-07-23 14:40:00 -0800

    Gianni's Gallery
    Elegant display of some of Gianni Vardanega's art

---
+++ gallery.js
@@ -0,0 +1,32 @@
+// GALLERY JS
+
+var walkR, walkL;
+
+function goRight() {
+	clearInterval(walkL);
+	walkR = setInterval(function(){ walkRight(); }, 10);
+	function walkRight() {
+		scrollBy(1,0);
+	};
+	document.getElementById("goRight").disabled = true;
+	document.getElementById("goLeft").disabled = false;
+};
+
+function goLeft() {
+	clearInterval(walkR);
+	walkL = setInterval(function(){ walkLeft(); }, 10);
+	function walkLeft() {
+		scrollBy(-1,0);
+	};
+	document.getElementById("goLeft").disabled = true;
+	document.getElementById("goRight").disabled = false;
+};
+
+
+function stop() {
+	clearInterval(walkR);
+	clearInterval(walkL);
+	document.getElementById("goRight").disabled = false;
+	document.getElementById("goLeft").disabled = false;
+};
+
