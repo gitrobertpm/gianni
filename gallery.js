23fb2277f051f4a6af6b7ebd4fe42bc59de417d5
Robert Manolis <robertpm@comcast.net>
2015-07-28 15:00:00 -0800

    updates
    first stage of horizontal scroll fix

--- gallery.js
+++ gallery.js
@@ -30,3 +30,34 @@ function stop() {
 	document.getElementById("goLeft").disabled = false;
 };
 
+document.addEventListener("DOMMouseScroll", stroll);
+document.addEventListener("mousewheel", stroll);
+document.addEventListener("wheel", stroll);
+
+function stroll() {
+	scrollBy(50,0);
+};
+
+//alert(window.getScrollOffsets());
+
+// var scrollDirArray = [];
+
+// function getScrollOffsets(w) {
+	// w = w || window;
+	// if (w.pageXOffset != null) return {x: w.pageXOffset, y:w.pageYOffset};
+// };
+
+
+
+//for (i = 0; i < getScrollOffsets().x.length)
+
+
+
+// if (delta < 0) {
+		// scrollBy(50,0);
+	// } else if (delta > 0) {
+		// scrollBy(-50,0);
+	// }
+
+//alert(wheelDelta);
+
